802bf9d95f3ecae8059c0b7d9dd6ca36f743fff7
Dustin Clark <138034989+Dclark2434@users.noreply.github.com>
2025-03-18 13:41:21 -0500

    refactored code and started working on get_character_count function

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+from stats import get_word_count
+from stats import get_character_count
+
 def get_book_text(book_path):
     with open(book_path) as f:
         contents = f.read()
     return contents
 
 def main():
-    return print(get_book_text("./books/frankenstein.txt"))
+    contents = get_book_text("./books/frankenstein.txt")
+    num_words = get_word_count(contents)
+    char_count = get_character_count(contents)
+    print(f"{num_words} words found in the document")
 
 main()
---
+++ stats.py
@@ -0,0 +1,10 @@
+def get_word_count(contents):
+    word_list = contents.split()
+    num_words = len(word_list)
+    return num_words
+
+def get_character_count(contents):
+    chars = {}
+    for char in char_list:
+        char_count.append()
+    
