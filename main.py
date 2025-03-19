9165defa94afb32ce6e6f0b994b2ca4ce7d85e0f
Dustin Clark <138034989+Dclark2434@users.noreply.github.com>
2025-03-19 18:16:32 -0500

    added sys.argv to accept an argument of book file to process

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from stats import get_word_count
 from stats import get_character_count
 from stats import get_sorted_char_counts
+import sys
+
+# Checks if there is a book parameter included in execution line and educates user.
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def get_book_text(book_path):
     with open(book_path) as f:
@@ -8,8 +14,8 @@ def get_book_text(book_path):
     return contents
 
 def main():
-    book_path = "books/frankenstein.txt"
-    contents = get_book_text(f"./{book_path}")
+    book_path = sys.argv[1]
+    contents = get_book_text(book_path)
     num_words = get_word_count(contents)
     char_count = get_character_count(contents)
     sorted_char_counts = get_sorted_char_counts(char_count)
